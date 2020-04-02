1e0e9f4e2ab7ee6c1d9c5d207e5303eba9cc9d2f
shr1ftyy <syeamtechdemon@gmail.com>
2020-04-02 11:43:03 +0100

    added video player for image data

---
+++ play_vid.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np 
+import os
+import time
+
+_dir = "data/images"
+files = os.listdir(_dir)
+convFiles = []
+
+for i in range(0, len(files)):
+    convFiles.append(int(files[i].split('.')[0]))
+
+convFiles.sort(reverse=False)
+
+for num in range(0, len(convFiles)):
+    convFiles[num] = str(convFiles[num])
+
+print(convFiles)
+
+os.chdir(_dir)
+for d in convFiles:
+    print(f'frame:{d}')
+    img = cv2.imread(f'{d}.png', -1)  
+    img = imS = cv2.resize(img, (int(np.shape(img)[1]/2),int(np.shape(img)[0]/2))) 
+    cv2.imshow(f'preview', img)
+
+    cv2.waitKey(20)
+
+exit()
